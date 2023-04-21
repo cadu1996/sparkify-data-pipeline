c28fe0a135ea52a4db0c7a23071842afda4351f4
Carlos Eduardo de Souza <carlos.eduardo.gba@gmail.com>
2023-04-21 01:43:14 -0300

    fix stage_redshift

--- plugins/operators/stage_redshift.py
+++ plugins/operators/stage_redshift.py
@@ -1,4 +1,4 @@
-from typing import List, Union
+from typing import List, Optional, Union
 
 from airflow.models import BaseOperator
 from airflow.providers.amazon.aws.hooks.redshift_sql import RedshiftSQLHook
@@ -14,7 +14,8 @@ class StageToRedshiftOperator(BaseOperator):
         schema (str): The schema that the table belongs to.
         table (str): The table to load the data into.
         s3_bucket (str): The S3 bucket containing the data.
-        s3_key (str): The S3 key of the data file.
+        s3_key (Optional[str], optional): The S3 key of the data file. Defaults
+            to None.
         redshift_conn_id (str, optional): The Redshift connection ID.
             Defaults to "redshift_default".
         aws_conn_id (str, optional): The AWS connection ID.
@@ -32,13 +33,15 @@ class StageToRedshiftOperator(BaseOperator):
 
     ui_color = "#358140"
 
+    template_fields = ("s3_bucket", "s3_key", "copy_options")
+
     def __init__(
         self,
         *,
         schema: str,
         table: str,
         s3_bucket: str,
-        s3_key: str,
+        s3_key: Optional[str] = None,
         redshift_conn_id: str = "redshift_default",
         aws_conn_id: str = "aws_default",
         verify: Union[bool, str, None] = None,
@@ -51,6 +54,8 @@ class StageToRedshiftOperator(BaseOperator):
 
         self.schema = schema
         self.table = table
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
         self.redshift_conn_id = redshift_conn_id
         self.aws_conn_id = aws_conn_id
         self.verify = verify
@@ -76,33 +81,35 @@ class StageToRedshiftOperator(BaseOperator):
         column_names = (
             "(" + ", ".join(self.column_list) + ")" if self.column_list else ""
         )
+        s3_key = self.s3_key if self.s3_key else ""
         return f"""
-            COPY {copy_destination} {column_names}
-            FROM 's3://{self.s3_bucket}/{self.s3_key}'
-            credentials '{credentials_block}'
-            {copy_options};
-        """
+        COPY {copy_destination} {column_names}
+        FROM 's3://{self.s3_bucket}/{s3_key}'
+        credentials '{credentials_block}'
+        {copy_options};
+    """
 
-    def execute(self, context):
-        """
-        Execute the StageToRedshiftOperator.
+def execute(self, context):
+    """
+    Execute the StageToRedshiftOperator.
 
-        Args:
-            context (dict): The execution context.
-        """
-        redshift_hook = RedshiftSQLHook(redshift_conn_id=self.redshift_conn_id)
+    Args:
+        context (dict): The execution context.
+    """
+    redshift_hook = RedshiftSQLHook(redshift_conn_id=self.redshift_conn_id)
 
-        s3_hook = S3Hook(aws_conn_id=self.aws_conn_id, verify=self.verify)
-        credentials = s3_hook.get_credentials()
-        credentials_block = build_credentials_block(credentials)
+    s3_hook = S3Hook(aws_conn_id=self.aws_conn_id, verify=self.verify)
+    credentials = s3_hook.get_credentials()
+    credentials_block = build_credentials_block(credentials)
 
-        copy_options = "\n\t\t\t".join(self.copy_options)
-        copy_destination = f"{self.schema}.{self.table}"
-        copy_statement = self._build_copy_query(
-            copy_destination, credentials_block, copy_options
-        )
+    copy_options = "\n\t\t\t".join(self.copy_options)
+    copy_destination = f"{self.schema}.{self.table}"
+    copy_statement = self._build_copy_query(
+        copy_destination, credentials_block, copy_options
+    )
 
-        self.log.info("Executing COPY command...")
-        redshift_hook.run(copy_statement, autocommit=self.autocommit)
+    self.log.info("Executing COPY command...")
+    redshift_hook.run(copy_statement, autocommit=self.autocommit)
 
-        self.log.info("COPY command complete...")
+    self.log.info("COPY command complete...")
+
