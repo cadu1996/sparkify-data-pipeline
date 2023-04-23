a2ac98a54010ec3fe3ea55206e32d5fb4cd3a02a
Carlos Eduardo de Souza <carlos.eduardo.gba@gmail.com>
2023-04-23 15:37:35 -0300

    DAGS working

---
+++ dags/create_tables_dag.py
@@ -0,0 +1,80 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+
+from airflow.providers.amazon.aws.operators.redshift_sql import RedshiftSQLOperator
+
+from helpers import SqlQueries
+
+
+default_args = {
+    "owner": "Carlos Eduardo de Souza",
+    "start_date": datetime(2019, 1, 12),
+}
+
+dag = DAG(
+    "create_tables_dag",
+    default_args=default_args,
+    description="Crate tables in Redshift with Airflow",
+    schedule_interval=None,
+    catchup=False,
+)
+
+start_operator = DummyOperator(task_id="Begin_execution", dag=dag)
+
+create_staging_events_table = RedshiftSQLOperator(
+    task_id="create_staging_events",
+    sql=SqlQueries.staging_events_table_create,
+    dag=dag,
+)
+
+create_staging_songs_table = RedshiftSQLOperator(
+    task_id="create_staging_songs",
+    sql=SqlQueries.staging_songs_table_create,
+    dag=dag,
+)
+
+create_songplays_table = RedshiftSQLOperator(
+    task_id="create_songplays_fact_table",
+    sql=SqlQueries.songplay_table_create,
+    dag=dag,
+)
+
+create_user_dimension_table = RedshiftSQLOperator(
+    task_id="create_user_dim_table",
+    sql=SqlQueries.user_table_create,
+    dag=dag,
+)
+
+create_song_dimension_table = RedshiftSQLOperator(
+    task_id="create_song_dim_table",
+    sql=SqlQueries.song_table_create,
+    dag=dag,
+)
+
+create_artist_dimension_table = RedshiftSQLOperator(
+    task_id="create_artist_dim_table",
+    sql=SqlQueries.artist_table_create,
+    dag=dag,
+)
+
+create_time_dimension_table = RedshiftSQLOperator(
+    task_id="create_time_dim_table",
+    sql=SqlQueries.time_table_create,
+    dag=dag,
+)
+
+end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+
+(
+    start_operator
+    >> [create_staging_events_table, create_staging_songs_table]
+    >> create_user_dimension_table
+    >> create_artist_dimension_table
+    >> create_time_dimension_table
+    >> create_song_dimension_table
+    >> create_songplays_table
+    >> end_operator
+)
+
---
+++ dags/etl_dag.py
@@ -0,0 +1,115 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from operators import (
+    StageToRedshiftOperator,
+    LoadFactOperator,
+    LoadDimensionOperator,
+    DataQualityOperator,
+)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    "owner": "Carlos Eduardo de Souza",
+    "start_date": datetime(2019, 1, 12),
+}
+
+dag = DAG(
+    "etl_dag",
+    default_args=default_args,
+    description="Load and transform data in Redshift with Airflow",
+    schedule_interval=None,
+    catchup=False,
+)
+
+start_operator = DummyOperator(task_id="Begin_execution", dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id="Stage_events",
+    table="staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data",
+    copy_options=[
+        "REGION 'us-west-2'",
+        "FORMAT AS JSON 's3://udacity-dend/log_json_path.json'",
+        "TIMEFORMAT AS 'epochmillisecs'",
+    ],
+    dag=dag,
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id="Stage_songs",
+    table="staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    copy_options=[
+        "REGION 'us-west-2'",
+        "FORMAT AS JSON 'auto'",
+    ],
+    dag=dag,
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id="Load_songplays_fact_table",
+    table="songplays",
+    query_transformation=SqlQueries.songplay_table_insert,
+    dag=dag,
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id="Load_user_dim_table",
+    table="users",
+    query_transformation=SqlQueries.user_table_insert,
+    truncate_table=True,
+    dag=dag,
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id="Load_song_dim_table",
+    table="songs",
+    query_transformation=SqlQueries.song_table_insert,
+    truncate_table=True,
+    dag=dag,
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id="Load_artist_dim_table",
+    table="artists",
+    query_transformation=SqlQueries.artist_table_insert,
+    truncate_table=True,
+    dag=dag,
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id="Load_time_dim_table",
+    table="time",
+    query_transformation=SqlQueries.time_table_insert,
+    truncate_table=True,
+    dag=dag,
+)
+
+run_quality_checks = DataQualityOperator(
+        task_id="Run_data_quality_checks",
+        tables=["songplays", "users", "songs", "artists", "time"],
+        dag=dag
+        )
+
+end_operator = DummyOperator(task_id="Stop_execution", dag=dag)
+
+(
+    start_operator
+    >> [stage_events_to_redshift, stage_songs_to_redshift]
+    >> load_songplays_table
+    >> [
+        load_user_dimension_table,
+        load_song_dimension_table,
+        load_artist_dimension_table,
+        load_time_dimension_table,
+    ]
+    >> run_quality_checks
+    >> end_operator
+)
